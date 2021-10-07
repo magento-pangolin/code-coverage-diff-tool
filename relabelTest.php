9d6a41afc75ac4ac78c876afffa6f73e3155e1a8
Kevin Kozan <kkozan@adobe.com>
2021-10-07 12:23:53 -0500

    Update relabelTest.php

--- relabelTest.php
+++ relabelTest.php
@@ -38,7 +38,6 @@ function readCoverageFromFolder($coveragePath, $outputDir, $newName) {
     $fileCount = count(scandir($coveragePath));
     $currentFile = 0;
     printf("Reading coverage...\n");
-    $debugflag = false;
 
     foreach (scandir($coveragePath) as $file) {
         printf("Reading ($currentFile/$fileCount)\r");
@@ -59,7 +58,6 @@ function readCoverageFromFolder($coveragePath, $outputDir, $newName) {
                 $testArray[] = $newName;
                 $newData[$testFile][$line] = $testArray;
             }
-
         }
         $newProcessedData = new \SebastianBergmann\CodeCoverage\ProcessedCodeCoverageData();
         $newProcessedData->setLineCoverage($newData);
@@ -67,14 +65,6 @@ function readCoverageFromFolder($coveragePath, $outputDir, $newName) {
         $newTestArray = [];
         $newTestArray[$newName] = ['size' => 'unknown', 'status' => 0, 'fromTestcase' => true];
         $fileCoverage->setTests($newTestArray);
-        if(!$debugflag) {
-            printf(key($fileCoverage->getData(true)->lineCoverage()));
-            printf("\n");
-            printf($fileCoverage->filter()->files()[0]);
-            printf("\n");
-
-            $debugflag = true;
-        }
         $writer = new SebastianBergmann\CodeCoverage\Report\PHP();
         $writer->process($fileCoverage, $outputDir.DIRECTORY_SEPARATOR.$file);
     }
